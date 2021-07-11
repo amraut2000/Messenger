3c4f7efb71ca5dd18d7f9bbe99344bb6ecb0955e
Ajay Raut <amraut2000@gmail.com>
2021-07-11 10:18:46 +0530

    users showed in the main activity

---
+++ app/src/main/java/com/example/messenger/ajay/Adapters/UsersAdapter.java
@@ -0,0 +1,55 @@
+package com.example.messenger.ajay.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.messenger.ajay.Models.User;
+import com.example.messenger.ajay.R;
+import com.example.messenger.ajay.databinding.MessageItemBinding;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder> {
+    ArrayList<User> users;
+    Context context;
+
+    public UsersAdapter(ArrayList<User> users, Context context) {
+        this.users = users;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.message_item,parent,false);
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersAdapter.UsersViewHolder holder, int position) {
+        User user=users.get(position);
+        holder.binding.userName.setText(user.getName());
+        Glide.with(context).load(user.getProfileImage())
+                .placeholder(R.drawable.avatar)
+                .into(holder.binding.userImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder{
+        MessageItemBinding binding;
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding=MessageItemBinding.bind(itemView);
+        }
+    }
+}
--- app/src/main/java/com/example/messenger/ajay/MainActivity.java
+++ app/src/main/java/com/example/messenger/ajay/MainActivity.java
@@ -3,19 +3,59 @@ package com.example.messenger.ajay;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.messenger.ajay.Adapters.UsersAdapter;
+import com.example.messenger.ajay.Models.User;
+import com.example.messenger.ajay.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
+    FirebaseDatabase firebaseDatabase;
+
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        users=new ArrayList<>();
+        usersAdapter=new UsersAdapter(users,this);
+        binding.usersRecyclerView.setAdapter(usersAdapter);
+
+        firebaseDatabase=FirebaseDatabase.getInstance();
+
+        firebaseDatabase.getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for(DataSnapshot snapshot1: snapshot.getChildren()){
+                    User user= snapshot1.getValue(User.class);
+                    users.add(user);
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Something wrong happened!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
